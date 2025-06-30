c5cd00cfb4d2334eafbf7374bcd5271b135c198d
sagor12388 <tonsagor@gmail.com>
2025-06-30 17:50:12 +0600

    Add files via upload

---
+++ Firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDR7VLGEbiW0aTzJ-2q7c6gm-LG3CMDZuM",
+  authDomain: "stream-the-show.firebaseapp.com",
+  projectId: "stream-the-show",
+  storageBucket: "stream-the-show.firebasestorage.app",
+  messagingSenderId: "1031800743760",
+  appId: "1:1031800743760:web:77412d1840c0ed426fee55",
+  measurementId: "G-DNDCT2LR50"
+};
+
+firebase.initializeApp(firebaseConfig);
